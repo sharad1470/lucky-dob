53d92e26c7273cc02312fe023bdceecc8e749c60
sharad1470 <sharadsingh3609@gmail.com>
2023-08-10 10:05:18 +0530

    logic part done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [dob,setDob]=useState("");
+  const [num,setNum]=useState(0);
+  const [result,setResult]=useState("");
+
+
+  const changeDob=(e)=>{
+    setDob(e.target.value);
+  }
+  const changeNum=(e)=>{
+    setNum(e.target.value);
+  }
+
+  const getnewDob=(dob)=>{
+
+    dob=dob.replaceAll('-',"");
+
+    let sum=0;
+    for(let i=0;i<dob.length;i++){
+      sum+=Number(dob[i]);
+    }
+    console.log(sum);
+    return sum;
+  }
+
+  const clickHandle=()=>{
+
+    if(dob!="" && num!=0){
+      const newDobSum=getnewDob(dob);
+      const ans=newDobSum%num;
+      if(ans===0)
+        setResult("you are lucky");
+      else  
+      setResult("you are not lucky");
+    }else{
+      setResult("put data correctly");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <div>
+     <label>Enter your dob:</label>
+     <input type='date' placeholder='dd/mm/yyy'
+     onChange={changeDob}
+     ></input>
+     </div>
+     <div>
+     <label>Enter your lucky number</label>
+     <input type='number'
+     onChange={changeNum}
+     ></input>
+     </div>
+     <div>
+     <button onClick={clickHandle}>Check here</button>
+     <div>{result}</div>
+     </div>
     </div>
   );
 }
